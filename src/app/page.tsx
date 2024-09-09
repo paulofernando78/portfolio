9eab21b803374e240b4bc1537ad01d6bb6120cc0
Paulo Fernando <paulofernando78@gmail.com>
2024-09-09 14:01:39 -0300

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,41 +5,48 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <main className="line-break">
-        <p className={styles["welcome"]}>Welcome to my space</p>
-        <div className={styles["flex-50px"]}>
-          <Card>
-            <div className="line-break">
-              <p>
-                <b>Web Projects</b>
-              </p>
-              <div>
-                <Link href="https://html-css-origamid.vercel.app/">
-                  <p>
-                    Lobo - (HTML/CSS){" "}
-                    <span className="p-size-small">(2023)</span>
-                  </p>
-                </Link>
-                <Link href="/">
-                  <p>
-                    BikCraft (HTML/CSS/JS)
-                    <span className="p-size-small">(2023)</span>
-                  </p>
-              </Link>
-              </div>
-            </div>
-          </Card>
-          <Card>
-            <div className="line-break">
-              <p>
-                <b>Music</b>
-              </p>
-              <Link href="https://www.instagram.com/alteredgravity/">
-                <p>Altered Gravity (instagram)</p>
-              </Link>
-            </div>
-          </Card>
+      <main>
+        <div className={styles["main-bg"]}>
+          <h1 className={styles["welcome"]}>Welcome to my space</h1>
         </div>
+          <div className="padding-10px">
+            <div className={styles["line-break-card"]}>
+              <Card>
+                <div className="line-break">
+                  <p>
+                    <b>WEB PROJECTS</b>
+                  </p>
+                  <div>
+                    <p>
+                      <b>Origamid</b>
+                    </p>
+                    <Link href="https://html-css-origamid.vercel.app/">
+                      <p>
+                        Lobo - (HTML/CSS){" "}
+                        <span className="p-size-small">(2023)</span>
+                      </p>
+                    </Link>
+                    <Link href="/">
+                      <p>
+                        BikCraft (HTML/CSS/JS)
+                        <span className="p-size-small">(2023)</span>
+                      </p>
+                    </Link>
+                  </div>
+                </div>
+              </Card>
+              <Card>
+                <div className="line-break">
+                  <p>
+                    <b>MUSIC</b>
+                  </p>
+                  <Link href="https://www.instagram.com/alteredgravity/">
+                    <p>Altered Gravity (instagram)</p>
+                  </Link>
+                </div>
+              </Card>
+            </div>
+          </div>
       </main>
     </>
   );
