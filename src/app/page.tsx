508e883df7713f49d0371e2c202d917cc93ac53b
Paulo Fernando <paulofernando78@gmail.com>
2024-09-12 11:00:00 -0300

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,63 +9,69 @@ export default function Home() {
         <div className={styles["main-bg"]}>
           <h1 className={styles["welcome"]}>Welcome to my space</h1>
         </div>
-          <div className="padding-10px">
-            <div className={styles["line-break-card"]}>
+        <div className="padding-10px">
+          <div className={styles["flex-50px"]}>
+            <div className="line-break">
               <Card>
-                <div className="line-break">
-                  <p>
-                    <b>WEB PROJECTS</b>
-                  </p>
-                  <div className="line-break">
-                    <div>
-                      <p>
-                        <b>Alura</b>
-                      </p>
-                      <a href="https://aluraplus-one-ashen.vercel.app/">
-                        <p>
-                          Alura+ - (HTML/CSS){" "}
-                          <span className="p-size-small">(2023)</span>
-                        </p>
-                      </a>
-                      <a href="https://alurabooks-fawn-sigma.vercel.app/">
-                        <p>
-                          Alura Book - (HTML/CSS){" "}
-                          <span className="p-size-small">(2023)</span>
-                        </p>
-                      </a>
-                    </div>
-                    <div>
-                      <p>
-                        <b>Origamid</b>
-                      </p>
-                      <a href="https://html-css-origamid.vercel.app/">
-                        <p>
-                          Lobo - (HTML/CSS){" "}
-                          <span className="p-size-small">(2023)</span>
-                        </p>
-                      </a>
-                      <a href="https://origamid-bikcraft-cyan.vercel.app/">
-                        <p>
-                          BikCraft (HTML/CSS/JS)
-                          <span className="p-size-small">(2023)</span>
-                        </p>
-                      </a>
-                    </div>
-                  </div>
-                </div>
+                <p>
+                  <b>WEB PROJECTS</b>
+                </p>
               </Card>
+              <div className="line-break">
+                <div>
+                  <p>
+                    <b>Alura</b>
+                  </p>
+                  <a href="https://aluraplus-one-ashen.vercel.app/">
+                    <p>
+                      Alura+ (HTML/CSS){" "}
+                      <span className="p-size-small">(2023)</span>
+                    </p>
+                  </a>
+                  <a href="https://alurabooks-fawn-sigma.vercel.app/">
+                    <p>
+                      Alura Book (HTML/CSS){" "}
+                      <span className="p-size-small">(2023)</span>
+                    </p>
+                  </a>
+                </div>
+                <div>
+                  <p>
+                    <b>Origamid</b>
+                  </p>
+                  <a href="https://html-css-origamid.vercel.app/">
+                    <p>
+                      Lobo (HTML/CSS){" "}
+                      <span className="p-size-small">(2023)</span>
+                    </p>
+                  </a>
+                  <a href="https://origamid-bikcraft-cyan.vercel.app/">
+                    <p>
+                      BikCraft (HTML/CSS/JS)
+                      <span className="p-size-small">(2023)</span>
+                    </p>
+                  </a>
+                  <a href="https://origamid-bikcraft-final.vercel.app/">
+                    <p>
+                      BikCraft Final (HTML/CSS/JS)
+                      <span className="p-size-small">(2024)</span>
+                    </p>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="line-break">
               <Card>
-                <div className="line-break">
-                  <p>
-                    <b>MUSIC</b>
-                  </p>
-                  <Link href="https://www.instagram.com/alteredgravity/">
-                    <p>Altered Gravity (instagram)</p>
-                  </Link>
-                </div>
+                <p>
+                  <b>MUSIC</b>
+                </p>
               </Card>
+              <Link href="https://www.instagram.com/alteredgravity/">
+                <p>Altered Gravity (instagram)</p>
+              </Link>
             </div>
           </div>
+        </div>
       </main>
     </>
   );
