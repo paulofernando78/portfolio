8e21f34cc6bcbeb99b653d91b63613f1bf4505d5
Paulo Silva <paulofernando78@gmail.com>
2025-11-18 03:37:12 -0300

    update

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,38 +1,13 @@
-import styleImports from "/src/css/imports.css?inline";
-
-const css = /*css*/ `
-  .card {
-    width: 100%;
-    height: 100%;
-    border: var(--border);
-    border-radius: var(--border-radius);
-    box-shadow: var(--box-shadow);
-    display: flex;
-    flex-direction: column;
-    overflow: hidden
-  }
-
-  .inner-card {
-    padding: 5px 5px 3px 8px;
-  }
-
-  .label {
-    font-weight: bold;
-    margin-bottom: 16px
-  }
-
-  iframe {
-    width: 100%;
-    height: 200px;
-    border: none;
-    aspect-ratio: 16 / 9;
-  }
-`;
+import styleImports from "/src/css/style.css?inline";
 
 class Card extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
+
+    const style = document.createElement("style");
+    style.textContent = styleImports;
+    this.shadowRoot.appendChild(style);
   }
 
   connectedCallback() {
@@ -42,14 +17,17 @@ class Card extends HTMLElement {
     const labelAttr = this.getAttribute("label");
     const descAttr = this.getAttribute("description");
 
-    this.shadowRoot.innerHTML = /* html*/ `
-    <style>${styleImports} ${css}</style>
-    <div class="card">
-      <img src="${imgAttr}"/>
-      <div class="inner-card">
-        <a href="${linkAttr}" class="label" >${labelAttr}</a>
-        <p>${descAttr}</p>     
+    const cardContainer = document.createElement("div");
+    cardContainer.className = "card__container";
+    this.shadowRoot.appendChild(cardContainer);
+
+    cardContainer.innerHTML = /* html*/ `
+      <div class="card__img-wrapper">
+        <img src="${imgAttr}"/>
       </div>
+      <div class="card__inner">
+        <a href="${linkAttr}" class="card__label" >${labelAttr}</a>
+        <p>${descAttr}</p>
     </div>
     `;
   }
